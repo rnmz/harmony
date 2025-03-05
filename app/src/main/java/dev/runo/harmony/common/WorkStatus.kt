2598c2596afc900073f7607a81fefd9029cab795
rnmz <naomiruno4@gmail.com>
2025-03-05 22:04:11 +0300

    small upd to workstatus.kt

--- app/src/main/java/dev/runo/harmony/common/WorkStatus.kt
+++ app/src/main/java/dev/runo/harmony/common/WorkStatus.kt
@@ -1,7 +1,7 @@
 package dev.runo.harmony.common
 
-sealed interface WorkStatus {
-    data class Success<T>(val data: T): WorkStatus
-    data class Error(val error: ErrorType, val message: String? = null): WorkStatus
-    data object Loading: WorkStatus
+sealed interface WorkStatus<T> {
+    data class Success<T>(val data: T): WorkStatus<T>
+    data class Error(val error: ErrorType, val message: String? = null): WorkStatus<Nothing>
+    data object Loading: WorkStatus<Nothing>
 }
